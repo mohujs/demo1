d41ffe63e0dd080929c00a67cd4992de6740232d
mohujs <mohujs@gmail.com>
2021-03-15 18:03:16 +0530

    Example Java

---
+++ ex.java
@@ -0,0 +1,27 @@
+
+class P{
+public static void main(String args[]){
+		System.out.println("Inside P");
+		main(10);
+	
+}
+
+public static void main(int a){
+		System.out.println("Inside Overloaded method");
+	
+}
+
+}
+
+class Q{
+public static void main(String args[]){
+	System.out.println("Inside Q");
+	
+}
+}
+
+class R{
+public static void main(String args[]){
+	System.out.println("Inside R ");
+}
+}
